4c473ea2edf9b8f906dce1cf639061dfd0fbaf60
lockerns <wzcong77@163.com>
2025-11-20 23:25:22 +0800

    Initial Commit

---
+++ SlideshowWallpaper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlideshowWallpaper
+//
+//  Created by Koko Lockerns on 2025/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlideshowWallpaper/SlideshowWallpaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideshowWallpaperApp.swift
+//  SlideshowWallpaper
+//
+//  Created by Koko Lockerns on 2025/11/20.
+//
+
+import SwiftUI
+
+@main
+struct SlideshowWallpaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
